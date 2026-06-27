b51baedf10b2676cebc99658e30a64846a784f30
Galoche et Patin <dev.admin@galocheetpatin.fr>
2026-06-27 16:22:14 +0200

    fix pressure plate state issues

--- 00_globals/global_level_manager.gd
+++ 00_globals/global_level_manager.gd
@@ -8,6 +8,7 @@ signal tile_map_bounds_changed(bounds: Array[Vector2])
 var current_tilemap_bounds: Array[Vector2]
 var target_transition: String
 var position_offset: Vector2
+var is_transitioning: bool = false
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -22,6 +23,7 @@ func load_new_level(
 	new_target_transition: String,
 	new_position_offset: Vector2
 ) -> void:
+	is_transitioning = true
 	get_tree().paused = true
 	target_transition = new_target_transition
 	position_offset = new_position_offset
@@ -31,5 +33,6 @@ func load_new_level(
 	get_tree().change_scene_to_file(level_path)
 	await SceneTransition.fade_in()
 	get_tree().paused = false
+	is_transitioning = false
 	await get_tree().process_frame
 	level_loaded.emit()
--- entities/props/interactables/dungeon/pressure_plate/pressure_plate.gd
+++ entities/props/interactables/dungeon/pressure_plate/pressure_plate.gd
@@ -8,38 +8,63 @@ signal deactivated
 @onready var audio: AudioStreamPlayer2D = $Audio
 @onready var audio_activate: AudioStream = preload("uid://cdttxq0ck6vdk")
 @onready var audio_deactivate: AudioStream = preload("uid://da1nerjhtmrbm")
+@onready var data_handler: PersistentDataHandler = $DataHandler
 
 var bodies_count: int = 0
 var is_active: bool = false
 var rect_offset: Rect2
 
 func _ready() -> void:
+	if data_handler:
+		data_handler.data_loaded.connect(_set_state)
 	if area:
 		area.body_entered.connect(_on_body_entered)
 		area.body_exited.connect(_on_body_exited)
 	if sprite:
 		rect_offset = sprite.region_rect
-
+	_set_state()
+	
 func _on_body_entered(_body: Node2D) -> void:
 	bodies_count += 1
-	check_is_activated()
+	_check_is_activated()
 
 func _on_body_exited(_body: Node2D) -> void:
+	if LevelManager.is_transitioning:
+		return
 	bodies_count -= 1
-	check_is_activated()
+	_check_is_activated()
 
-func check_is_activated() -> void:
+func _activate() -> void:
+	is_active = true
+	data_handler.set_value("is_active", is_active)
+	_update_texture()
+	_play_audio(audio_activate)
+	activated.emit()
+
+func _deactivate() -> void:
+	is_active = false
+	data_handler.set_value("is_active", is_active)
+	_update_texture()
+	_play_audio(audio_deactivate)
+	deactivated.emit()
+
+func _check_is_activated() -> void:
+	is_active = data_handler.get_value("is_active") or false
 	if is_active == false and bodies_count > 0:
-		is_active = true
-		sprite.region_rect.position.x = rect_offset.position.x - sprite.region_rect.size.x
-		play_audio(audio_activate)
-		activated.emit()
+		_activate()
 	elif is_active == true and bodies_count <= 0:
-		is_active = false
-		sprite.region_rect.position.x = rect_offset.position.x
-		play_audio(audio_deactivate)
-		deactivated.emit()
+		_deactivate()
 
-func play_audio(stream: AudioStream) -> void:
+func _play_audio(stream: AudioStream) -> void:
 	audio.stream = stream
 	audio.play()
+
+func _update_texture() -> void:
+	if is_active:
+		sprite.region_rect.position.x = rect_offset.position.x - sprite.region_rect.size.x
+	else:
+		sprite.region_rect.position.x = rect_offset.position.x
+
+func _set_state() -> void:
+	is_active = data_handler.get_value("is_active") or false
+	_update_texture()
