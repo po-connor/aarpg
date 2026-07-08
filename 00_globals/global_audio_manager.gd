4caf7f57364766b183e0541c7db2c80c647ebba0
Galoche et Patin <dev.admin@galocheetpatin.fr>
2026-07-08 12:33:40 +0200

    remove prints

--- 00_globals/global_audio_manager.gd
+++ 00_globals/global_audio_manager.gd
@@ -8,7 +8,6 @@ var music_fade_duration: float = 0.5
 var music_fade_volume: float = -40.0
 
 func _ready() -> void:
-	print("Global audio ready!")
 	for i in music_audio_player_count:
 		var audio_stream_player: AudioStreamPlayer = AudioStreamPlayer.new()
 		audio_stream_player.name = "audio_player_" + str(i)
@@ -37,7 +36,6 @@ func play_music(audio: AudioStream) -> void:
 func _fade_in(music_player: AudioStreamPlayer, audio: AudioStream) -> void:
 	if not music_player or not audio:
 		return
-	print("Music fade in : ", music_player.name)
 	music_player.stream = audio
 	music_player.volume_db = music_fade_volume
 	music_player.play()
@@ -47,17 +45,6 @@ func _fade_in(music_player: AudioStreamPlayer, audio: AudioStream) -> void:
 func _fade_out(music_player: AudioStreamPlayer ) -> void:
 	if not music_player:
 		return
-	print("Music fade out : ", music_player.name)
 	var tween: Tween = get_tree().create_tween()
 	await tween.tween_property(music_player, "volume_db", 0.0, music_fade_duration).finished
 	music_player.stop()
-
-#
-#func _transition(out_music_player_index: int, in_music_player_index: int, audio: AudioStream) -> void:
-	#print("_transition")
-	#if music_players[out_music_player_index].stream == audio:
-		#return
-	#var tween: Tween = get_tree().create_tween()
-	#tween.tween_property(music_players[out_music_player_index], "volume_db", music_fade_volume, music_fade_duration)
-	#tween.tween_callback(music_players[out_music_player_index].stop)
-	#tween.tween_callback(_fade_in.bind(in_music_player_index, audio))
